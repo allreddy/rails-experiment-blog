96b574a3c593bfbfb7123c3e39a684946ef879ee
allreddy <nithin@enplug.com>
2018-12-02 16:36:00 -0800

    routes with ability to create, read, update

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(articles_params)
+
+        if @article.save
+            redirect_to @article
+        else 
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        @article.update(articles_params)
+
+        if @article.save
+            redirect_to @article
+        else
+            render 'edit'
+        end
+
+    end
+
+    private
+    
+        def articles_params
+            params.require(:article).permit(:text, :title)
+        end
+
+end
